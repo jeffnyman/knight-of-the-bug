ac73de7cae736fe2e6efed4a3295a031e947826e
Jeff Nyman <jeffnyman@gmail.com>
2023-09-01 19:04:44 -0500

    Adding babel transpiler to the package.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
+import babel from "@rollup/plugin-babel";
+
 export default {
   input: "./src/scripts/app.js",
+
   output: {
     file: "./app/dist/app.js",
     format: "es",
     sourcemap: "inline",
   },
+
+  plugins: [babel({ babelHelpers: "bundled" })],
 };
